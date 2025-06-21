33339c450bab59e1847b69d096b9228b7a6ea775
Wilsons-Navid <wadotiwawil@gmail.com>
2025-06-21 15:38:05 +0300

    poisson.py

---
+++ math/probability/poisson.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+'''
+    Create a class that represents the poisson distribution
+'''
+
+
+class Poisson():
+    """ This class represents a poisson distribution """
+
+    def __init__(self, data=None, lambtha=1.):
+        """ initialization method """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.lambtha = float(sum(data) / len(data))
+
+    def factorial(self, num):
+        """ Calculates num!"""
+        values = [num for num in range(1, num+1)]
+        factorial = 1
+        if num == 0:
+            return 1
+        for value in values:
+            factorial *= value
+        return factorial
+
+    def pmf(self, k):
+        """ Calculates the value of the PMF for a given number of successes"""
+        # assigning the value of euler's number to a variable e
+        e = 2.7182818285
+        if not isinstance(k, int):
+            k = int(k)
+        if k <= 0:
+            return 0
+        return e ** -(self.lambtha) * (self.lambtha)**k / self.factorial(k)
+
+    def cdf(self, k):
+        """ calculates the cumulative distribution"""
+        e = 2.7182818285
+        if not isinstance(k, int):
+            k = int(k)
+        if k <= 0:
+            return 0
+        sample_space = [i for i in range(0, k+1)]
+        cdf = 0
+        for i in sample_space:
+            cdf += e ** -(self.lambtha) * (self.lambtha)**i / self.factorial(i)
+        return cdf
