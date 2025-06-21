752dc2bd14556f1c582a4eb3fea1f9252ecce82f
Wilsons-Navid <wadotiwawil@gmail.com>
2025-06-21 15:43:49 +0300

    exponential

---
+++ math/probability/exponential.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+'''
+    Exponential class
+'''
+
+
+class Exponential():
+    """ Represents an exponential distribution """
+
+    def __init__(self, data=None, lambtha=1.):
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.lambtha = 1 / float(sum(data) / len(data))
+
+    def pdf(self, x):
+        """ calculates the probability density function """
+        # assigning the value of euler's number to a variable e
+        e = 2.7182818285
+        if x < 0:
+            return 0
+        return self.lambtha * (e ** (-(self.lambtha) * x))
+
+    def cdf(self, x):
+        """ Returns the value of the cdf """
+        e = 2.7182818285
+        if x < 0:
+            return 0
+        return 1 - (e ** (-(self.lambtha) * x))
